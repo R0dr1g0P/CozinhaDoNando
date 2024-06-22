1144a0bcdeb708a4a034f74e8b01a62787ef9886
R0dr1g0P <rodrigojcpinto2004@gmail.com>
2024-06-23 00:40:46 +0100

    Compor detalhes visuais do que já estava realizado e novo texto (Lista de Receitas)

--- app/src/main/java/com/example/cozinhadonando/MainActivity.kt
+++ app/src/main/java/com/example/cozinhadonando/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
@@ -17,9 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.cozinhadonando.ui.theme.CozinhaDoNandoTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,19 +47,29 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize().padding(16.dp)
     ) {
         Row(
-            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.padding(bottom = 16.dp)
         ) {
             Text(
                 text = "As Receitas do Chef Nando",
-                style = MaterialTheme.typography.headlineLarge,
+                style = MaterialTheme.typography.headlineLarge.copy(fontSize = 35.sp, fontWeight = FontWeight.Bold),
                 textAlign = TextAlign.Center,
                 modifier = Modifier.weight(3f)
             )
             Image(
                 painter = painterResource(id = R.drawable.chapeu_cozinheiro),
                 contentDescription = "Chapéu de Cozinheiro",
-                modifier = Modifier.weight(1f).size(100.dp)
+                modifier = Modifier.weight(1f).size(120.dp)
+            )
+        }
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(10.dp)
+        ) {
+            Text(
+                text = "Lista de Receitas:",
+                style = MaterialTheme.typography.bodyLarge.copy(fontSize = 25.sp),
+                textAlign = TextAlign.Center
             )
         }
     }
@@ -69,3 +82,4 @@ fun GreetingPreview() {
         MainScreen()
     }
 }
+
