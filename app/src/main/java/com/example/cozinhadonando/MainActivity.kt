87fd6a96632f87662aa7334ecad55f2b5c4d10af
R0dr1g0P <rodrigojcpinto2004@gmail.com>
2024-06-22 22:40:19 +0100

    Cabeçalho da "MainScreen()" com o Título do programa e uma imagem de chapéu de cozinheiro

--- app/src/main/java/com/example/cozinhadonando/MainActivity.kt
+++ app/src/main/java/com/example/cozinhadonando/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.cozinhadonando
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cozinhadonando.ui.theme.CozinhaDoNandoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CozinhaDoNandoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +39,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
+        Row(
+            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 16.dp)
+        ) {
+            Text(
+                text = "As Receitas do Chef Nando",
+                style = MaterialTheme.typography.headlineLarge,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.weight(3f)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.chapeu_cozinheiro),
+                contentDescription = "Chapéu de Cozinheiro",
+                modifier = Modifier.weight(1f).size(100.dp)
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CozinhaDoNandoTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
